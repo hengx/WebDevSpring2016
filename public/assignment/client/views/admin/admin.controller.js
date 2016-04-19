41c63893221114b8f105b3f99cd6ceebf3e63b80
Heng Xu <xu.he@husky.neu.edu>
2016-04-19 09:47:08 -0700

    fixed the remove and add for admin

--- public/assignment/client/views/admin/admin.controller.js
+++ public/assignment/client/views/admin/admin.controller.js
@@ -4,7 +4,7 @@
     angular.module("FormBuilderApp")
         .controller("AdminController", AdminController);
 
-    function AdminController(UserService, $scope) {
+    function AdminController(UserService,$scope) {
 
         var vm = this;
         vm.add = add;
@@ -29,7 +29,7 @@
             UserService
                 .deleteUserById(user._id)
                 .then(function (response) {
-                    vm.users.splice(index, 1);
+                    $scope.users.splice(index, 1);
                 }, handleError);
             //.then(handleSuccess, handleError);
         }
@@ -42,10 +42,10 @@
             UserService
                 .updateUser(user._id, user)
                 .then(function (response) {
-                    for (var u in vm.users) {
-                        if (vm.users[u]._id === user._id) {
+                    for (var u in $scope.users) {
+                        if ($scope.users[u]._id === user._id) {
                             user.roles = user.roles.toString();
-                            vm.users[u] = user;
+                            $scope.users[u] = user;
                         }
                     }
                 }, handleError);
@@ -53,73 +53,27 @@
 
         }
 
-        //UserService
-        //    .updateUser(selectedUser._id, selectedUser)
-        //    //.then(function(users){
-        //    $scope.users = users;
-        //    $scope.selectedUser.username = "";
-        //    $scope.selectedUser.password = "";
-        //    $scope.selectedUser.firstName = "";
-        //    $scope.selectedUser.lastName = "";
-        //    $scope.selectedUser.roles = "";
-        //    delete $scope.selectedUser._id;
-        //});
-
-
-        //if (user !== undefined){
-        //        if (user.roles !== undefined && user._id !== undefined) {
-        //            user.roles = user.roles.split(',').map(function(string) {return string.trim();});
-        //        }
-        //
-        //        UserService
-        //            .updateUser(selectedUser._id, user)
-        //            .then(handleSuccess, handleError);
-        //    }
-        //
-        //}
-
         function add(user) {
             console.log("add user");
             console.log(user);
-            //UserService
-            //    .createUser(selectedUser)
-            //    .then(function(user){
-            //        $scope.user = user;
-            //    });
 
-            //if (selectedUser !== undefined) {
-            //    if (selectedUser.roles !== undefined) {
-            //        selectedUser.roles = selectedUser.roles.split(',').map(function (string) {
-            //            return string.trim();
-            //        });
-            //    }
             if (user.roles) {
                 user.roles = user.roles.split(',');
             }
             UserService
                 .createUser(user)
-                //.then(handleSuccess, handleError);
                 .then(function (response) {
                     if (response.data !== undefined) {
-                        vm.users.push(response.data);
+                        $scope.users.push(response.data);
                     }
                 });
             user.roles = user.roles.join();
-            //.then(function(users){
-            //    $scope.users = users;
-            //})
-            //}
+
         }
 
         function select(user) {
             console.log("select user");
             console.log(user);
-            //UserService
-            //    .findUserById(user._id)
-            //    .then(function(u){
-            //        $scope.selectedUser = u;
-            //        $scope.selectedUser.password = '';
-            //    });
 
             user.roles = user.roles.toString();
 
@@ -127,26 +81,13 @@
             vm.user.password = '';
         }
 
-        //function handleResponse(response) {
-        //    $scope.users = response.data.map(function(user) {
-        //        if (user.roles !== undefined) {
-        //            user.roles = user.roles.join(', ');
-        //        } else {
-        //            user.roles = '';
-        //        }
-        //        return user;
-        //    });
-        //    $scope.selectedUser = undefined;
-        //}
-        //
-        //
         function handleSuccess(response) {
             //init();
             console.log(response.data);
             for (var i in response.data) {
                 response.data[i].roles = response.data[i].roles.toString();
             }
-            $scope.users = response.data;
+        $scope.users = response.data;
         }
 
         function handleError(error) {
